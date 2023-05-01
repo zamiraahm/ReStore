3ee9f4606a942071134198f985b5c3536e4a3fad
lizableta <bletaeliza@gmail.com>
2023-05-01 13:55:10 +0200

    React router

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import Catalog from "../../features/catalog/Catalog";
 import { Container, CssBaseline, createTheme } from "@mui/material";
 import Header from "./Header";
 import { ThemeProvider } from "@emotion/react";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode]=useState(false);
@@ -25,7 +26,7 @@ function App() {
     <CssBaseline/>
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-      <Catalog />
+      <Outlet/>
       </Container>
       
    </ThemeProvider>
